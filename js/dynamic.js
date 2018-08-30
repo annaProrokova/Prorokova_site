3e296739f8c1412423077aa561c0dc57370b493d
anna_prorokova <anna.prorokova@mail.ru>
2018-08-31 00:41:11 +0700

    First commit

---
+++ js/dynamic.js
@@ -0,0 +1,24 @@
+//комментарий
+
+
+/*var age = 10;
+
+var sum = age + 7;
+console.log(sum);
+// if(age < 0)*/
+//var i=0;
+/*do{
+  console.log(i);
+  i++;
+} while (i>10);*/
+//for(i=0;i<10;i++){
+  //if(i==5){console.log('Привет');
+//continue;}
+//console.log(i);
+
+
+
+x=eval(calc.x.value);
+alert(x);
+var a = +prompt("Введите первое число", "");
+var b = +prompt("Введите второе число", "");
